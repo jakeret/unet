86d240ac21430333a249771c3f2ef67e70764952
Akeret, Joel <joel.akert@gmail.com>
2020-03-26 14:19:41 +0100

    fixed architecture

--- src/unet/unet.py
+++ src/unet/unet.py
@@ -2,27 +2,26 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import Model, Input
 from tensorflow.keras import layers
-from tensorflow.keras import initializers
+from tensorflow.keras.initializers import TruncatedNormal
 import numpy as np
 
-
-def conv_layer(x, layer_idx, filters_root, kernel_size, dropout_rate=0.5):
+def conv_block(x, layer_idx, filters_root, kernel_size, dropout_rate):
     with tf.name_scope("conv_layer"):
         filters = 2 ** layer_idx * filters_root
         stddev = np.sqrt(2 / (kernel_size ** 2 * filters))
 
-        x = layers.Conv2D(filters=filters // 2,
-                                      kernel_size=(kernel_size, kernel_size),
-                                      kernel_initializer=initializers.TruncatedNormal(stddev=stddev),
-                                      strides=1,
-                                      padding="valid")(x)
+        x = layers.Conv2D(filters=filters,
+                          kernel_size=(kernel_size, kernel_size),
+                          kernel_initializer=TruncatedNormal(stddev=stddev),
+                          strides=1,
+                          padding="valid")(x)
         x = layers.Dropout(rate=dropout_rate)(x)
         x = layers.Activation("relu")(x)
 
         x = layers.Conv2D(filters=filters,
-                                      kernel_size=(kernel_size, kernel_size),
-                                      strides=1,
-                                      padding="valid")(x)
+                          kernel_size=(kernel_size, kernel_size),
+                          strides=1,
+                          padding="valid")(x)
         x = layers.Dropout(rate=dropout_rate)(x)
         x = layers.Activation("relu")(x)
 
@@ -34,21 +33,20 @@ def max_pool_layer(x, pool_size):
     return x
 
 
-def upconv_layer(x, layer_idx, filters_root, kernel_size):
-    filters = 2 ** layer_idx * filters_root
+def upconv_block(x, layer_idx, filters_root, kernel_size, pool_size):
+    filters = 2 ** (layer_idx + 1) * filters_root
     stddev = np.sqrt(2 / (kernel_size ** 2 * filters))
-    x = layers.Conv2DTranspose(filters,
-                               kernel_size=(kernel_size, kernel_size),
-                               kernel_initializer=initializers.TruncatedNormal(
-                                   stddev=stddev),
-                               strides=kernel_size,
+    x = layers.Conv2DTranspose(filters // 2,
+                               kernel_size=(pool_size, pool_size),
+                               kernel_initializer=TruncatedNormal(stddev=stddev),
+                               strides=pool_size,
                                padding="valid"
                                )(x)
     x = layers.Activation("relu")(x)
     return x
 
 
-def crop_layer(x, down_layer):
+def crop_concat_block(x, down_layer):
     with tf.name_scope("crop_layer"):
         x1_shape = tf.shape(down_layer)
         x2_shape = tf.shape(x)
@@ -62,7 +60,14 @@ def crop_layer(x, down_layer):
         return x
 
 
-def build_model(channels:int, n_class:int, layer_depth:int, filters_root:int, kernel_size=3, pool_size=2) -> Model:
+def build_model(channels:int,
+                num_classes:int,
+                layer_depth:int,
+                filters_root:int,
+                kernel_size:int=3,
+                pool_size:int=2,
+                dropout_rate:int=0.5) -> Model:
+
     inputs = Input(shape=(None, None, channels))
 
     x = inputs
@@ -71,28 +76,28 @@ def build_model(channels:int, n_class:int, layer_depth:int, filters_root:int, ke
     with tf.name_scope("contracting"):
         for layer_idx in range(0, layer_depth - 1):
             with tf.name_scope(f"contracting_{layer_idx}"):
-                x = conv_layer(x, layer_idx, filters_root, kernel_size)
+                x = conv_block(x, layer_idx, filters_root, kernel_size, dropout_rate)
                 down_layers[layer_idx] = x
                 x = max_pool_layer(x, pool_size)
 
     with tf.name_scope("bottom"):
-        x = conv_layer(x, layer_idx + 1, filters_root, kernel_size)
+        x = conv_block(x, layer_idx + 1, filters_root, kernel_size, dropout_rate)
 
     with tf.name_scope("expanding"):
         for layer_idx in range(layer_depth - 2, -1, -1):
             with tf.name_scope(f"expanding_{layer_idx}"):
-                x = upconv_layer(x, layer_idx, filters_root, pool_size)
-                x = crop_layer(x, down_layers[layer_idx])
-                x = conv_layer(x, layer_idx, filters_root, kernel_size)
+                x = upconv_block(x, layer_idx, filters_root, kernel_size, pool_size)
+                x = crop_concat_block(x, down_layers[layer_idx])
+                x = conv_block(x, layer_idx, filters_root, kernel_size, dropout_rate)
 
-    stddev = np.sqrt(2 / (kernel_size ** 2 * filters_root))
-    x = layers.Conv2D(filters=n_class,
+    stddev = np.sqrt(2 / (kernel_size ** 2 * filters_root * 2))
+    x = layers.Conv2D(filters=num_classes,
                       kernel_size=(1, 1),
-                      kernel_initializer=initializers.TruncatedNormal(stddev=stddev),
+                      kernel_initializer=TruncatedNormal(stddev=stddev),
                       strides=1,
                       padding="valid"
                       )(x)
-
+    x = layers.Activation("relu")(x)
     outputs = layers.Activation("softmax")(x)
     model = Model(inputs, outputs)
     return model
