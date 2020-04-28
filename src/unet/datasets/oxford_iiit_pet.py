27f541a5c8e04796627ea6999df2f13a99f0108d
Akeret, Joel <joel.akert@gmail.com>
2020-04-28 11:33:03 +0200

    fixed formatting

--- src/unet/datasets/oxford_iiit_pet.py
+++ src/unet/datasets/oxford_iiit_pet.py
@@ -9,32 +9,34 @@ IMAGE_SIZE = (128, 128)
 channels = 3
 classes = 3
 
+
 def normalize(input_image, input_mask):
-  input_image = tf.cast(input_image, tf.float32) / 255.0
-  input_mask -= 1
-  return input_image, input_mask
+    input_image = tf.cast(input_image, tf.float32) / 255.0
+    input_mask -= 1
+    return input_image, input_mask
 
 
 def load_image_train(datapoint):
-  input_image = tf.image.resize(datapoint['image'], IMAGE_SIZE)
-  input_mask = tf.image.resize(datapoint['segmentation_mask'], IMAGE_SIZE)
+    input_image = tf.image.resize(datapoint['image'], IMAGE_SIZE)
+    input_mask = tf.image.resize(datapoint['segmentation_mask'], IMAGE_SIZE)
 
-  if tf.random.uniform(()) > 0.5:
-    input_image = tf.image.flip_left_right(input_image)
-    input_mask = tf.image.flip_left_right(input_mask)
+    if tf.random.uniform(()) > 0.5:
+        input_image = tf.image.flip_left_right(input_image)
+        input_mask = tf.image.flip_left_right(input_mask)
 
-  input_image, input_mask = normalize(input_image, input_mask)
+    input_image, input_mask = normalize(input_image, input_mask)
 
-  return input_image, input_mask
+    return input_image, input_mask
 
 
 def load_image_test(datapoint):
-  input_image = tf.image.resize(datapoint['image'], IMAGE_SIZE)
-  input_mask = tf.image.resize(datapoint['segmentation_mask'], IMAGE_SIZE)
+    input_image = tf.image.resize(datapoint['image'], IMAGE_SIZE)
+    input_mask = tf.image.resize(datapoint['segmentation_mask'], IMAGE_SIZE)
 
-  input_image, input_mask = normalize(input_image, input_mask)
+    input_image, input_mask = normalize(input_image, input_mask)
 
-  return input_image, input_mask
+    return input_image, input_mask
+
 
 def load_data(buffer_size=1000, **kwargs) -> Tuple[tf.data.Dataset, tf.data.Dataset]:
     dataset, info = tfds.load('oxford_iiit_pet:3.*.*', with_info=True, **kwargs)
