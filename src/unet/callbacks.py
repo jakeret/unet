89adb8105b4e3ee59e0c4e478676489f12d19dda
Akeret, Joel <joel.akeret@gmail.com>
2020-05-18 11:38:04 +0200

    simplify subclassing

--- src/unet/callbacks.py
+++ src/unet/callbacks.py
@@ -40,22 +40,38 @@ class TensorBoardImageSummary(Callback):
                                               .map(utils.crop_image_and_label_to_shape(predictions.shape[1:]))
                                               .take(self.max_outputs)
                                               .batch(self.max_outputs))[0]
-        if predictions.shape[-1] == 2:
-            mask = predictions[..., :1]
 
-        else:
-            mask = np.argmax(predictions, axis=-1)[..., np.newaxis]
-
-        output = np.concatenate((utils.to_rgb(cropped_images.numpy()),
-                                 utils.to_rgb(cropped_labels[..., :1].numpy()),
-                                 utils.to_rgb(mask)),
-                                axis=2)
+        output = self.combine_to_image(cropped_images.numpy(),
+                                       cropped_labels.numpy(),
+                                       predictions)
 
         with self.file_writer.as_default():
             tf.summary.image(self.name,
                              output,
                              step=epoch,
                              max_outputs=self.max_outputs)
+
+    def combine_to_image(self, images: np.array, labels: np.array, predictions: np.array) -> np.array:
+        """
+        Concatenates the three tensors to one RGB image
+
+        :param images: images tensor, shape [None, nx, ny, channels]
+        :param labels: labels tensor, shape [None, nx, ny, 1] for sparse or [None, nx, ny, classes] for one-hot
+        :param predictions: labels tensor, shape [None, nx, ny, classes]
+
+        :return: image tensor, shape [None, nx, 3 x ny, 3]
+        """
+
+        if predictions.shape[-1] == 2:
+            mask = predictions[..., :1]
+        else:
+            mask = np.argmax(predictions, axis=-1)[..., np.newaxis]
+
+        output = np.concatenate((utils.to_rgb(images),
+                                 utils.to_rgb(labels[..., :1]),
+                                 utils.to_rgb(mask)),
+                                axis=2)
+        return output
 
     def _log_histogramms(self, epoch, predictions):
         with self.file_writer.as_default():
